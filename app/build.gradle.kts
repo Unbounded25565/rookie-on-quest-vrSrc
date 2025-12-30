0c7d8011e7f726d169dbae7c894a17bba6d66e0f
LeGeRyChEeSe <kilian.douarinou41@gmail.com>
2025-12-30 02:01:08 +0100

    Release v2.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.vrpirates.rookieonquest"
         minSdk = 29
         targetSdk = 34
-        versionCode = 5
-        versionName = "2.2.0"
+        versionCode = 6
+        versionName = "2.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
